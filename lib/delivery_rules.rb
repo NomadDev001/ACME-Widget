89ee712852402a5f28d82e1d7619c6e6ad920ce6
Nomad <danielkaczmarek13@outlook.com>
2026-07-02 22:59:26 +0200

    Implement delivery charge calculation

--- lib/delivery_rules.rb
+++ lib/delivery_rules.rb
@@ -1,2 +1,8 @@
 class DeliveryRules
+  def charge_for(subtotal)
+    return 0.00 if subtotal >= 90
+    return 2.95 if subtotal >= 50
+
+    4.95
+  end
 end
--- spec/delivery_rules_spec.rb
+++ spec/delivery_rules_spec.rb
@@ -1,7 +1,9 @@
 require_relative "../lib/delivery_rules"
 
 RSpec.describe DeliveryRules do
-  it "exists" do
-    expect(DeliveryRules).to be_a(Class)
+  it "charges 4.95 when subtotal is under 50" do
+    rules = DeliveryRules.new
+
+    expect(rules.charge_for(32.90)).to eq(4.95)
   end
 end
